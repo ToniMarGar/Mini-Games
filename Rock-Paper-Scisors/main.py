c3caa01e92565a7b7b4d8e2832e66ad8330e318f
ToniMarGar <amartin7893@gmail.com>
2024-10-14 10:33:06 +0100

    Copy

--- Rock-Paper-Scisors/main.py
+++ Rock-Paper-Scisors/main.py
@@ -1,17 +1,30 @@
 import random
+import user_functions
 
 count_deuce = 0
 count_wins = 0
 count_loses = 0
 
-posibilities = ['Rock', 'Paper', 'Scisors']
-print("Rock, Paper or Scisors? ")
-choice_User = input("What do you choose? ")
+total_wins = 0
+total_loses = 0
 
+options = ['Rock', 'Paper', 'Scissors']
+print("Rock, Paper or Scissors")
+choice_User = input("What do you choose? ")
+   
 for i in range(1):
-   choice_PC = random.choice(posibilities)
-    
+   choice_PC = random.choice(options)
+   
 if choice_PC == choice_User:
    count_deuce += 1
 
-print(count_deuce)
+def user_rock():
+   if choice_PC == "Paper":
+      count_loses += 1
+   elif choice_PC == "Scissors":
+      count_wins += 1
+   if choice_PC == "Rock":
+      count_deuce += 1
+
+if choice_User == "Rock":
+   user_rock()
